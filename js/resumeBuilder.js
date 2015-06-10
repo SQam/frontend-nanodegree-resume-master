0dc4be82a347c84ea9dcb6fa095bf75e5e2cd788
SQam <khanz@usxxkhanzm1.corp.emc.com>
2015-06-10 15:52:24 -0400

    Printing out Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,10 @@
- var firstName="Serap"
-console.log(firstName)
+ var name= "Serap"
+ var Role= "Web Developer"
+ var formattedName= HTMLheaderName.replace( "%data%", name);
+ var formattedRole= HTMLheaderRole.replace( "%data%", Role);
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+
+
+
+
